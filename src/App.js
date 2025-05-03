24f15610cef04b607cb20d93c20f889b385ef43a
Aravind K Krishna <ananthkkrishna2001@gmail.com>
2025-05-03 23:51:28 +0530

    day1 intialised and installed libraries

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { Routes,Route } from 'react-router-dom';
+//react slike
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" />
+      <Route path="/movie/:id" />
+      <Route path="/plays" /> */}
+    </Routes>
+  );
+}
+
+export default App;
